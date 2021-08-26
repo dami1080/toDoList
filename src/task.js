1782ed95091343efea6386d64f960517e8139ad9
dami1080 <dareakintoye@gmail.com>
2021-08-26 09:49:22 -1100

    created a function to edit task

--- src/task.js
+++ src/task.js
@@ -33,3 +33,26 @@ export function removeAll(arr) {
   window.localStorage.setItem('tasklist', JSON.stringify(arr));
   window.location.reload();
 }
+
+export function edit(arr) {
+  arr.forEach((element) => {
+    const description = document.getElementById(`${element.index}-description`);
+    const dragBtn = document.getElementById(`${element.index}-drag`);
+    const trashBtn = document.getElementById(`${element.index}-trash`);
+    description.addEventListener('click', () => {
+      description.setAttribute('contenteditable', true);
+      description.classList.add('editing');
+      dragBtn.style.display = 'none';
+      trashBtn.style.display = 'block';
+    });
+    document.addEventListener('dblclick', () => {
+      description.setAttribute('contenteditable', false);
+      description.classList.remove('editing');
+      dragBtn.style.display = 'block';
+      trashBtn.style.display = 'none';
+      element.description = description.innerHTML;
+      window.localStorage.setItem('tasklist', JSON.stringify(arr));
+      window.location.reload();
+    });
+ });
+}
